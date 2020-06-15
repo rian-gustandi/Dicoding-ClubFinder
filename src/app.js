8f391d79df3e881b299a70ced293888bf6df60ad
rian-gustandi <rian.gustandi17@gmail.com>
2020-06-15 20:12:32 +0700

    Change bundle.js , app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 import "regenerator-runtime";
 import "./styles/style.css";
 import "./script/component/app-bar.js";
+
 import main from "./script/view/main.js";
 
 document.addEventListener("DOMContentLoaded", main);
